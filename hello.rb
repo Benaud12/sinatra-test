702b0c337103c5cfa83c8b69d515b2b160af14ce
zsid <zhivko.siderov@gmail.com>
2015-09-14 16:20:51 +0100

    Sinatra 6

--- hello.rb
+++ hello.rb
@@ -4,3 +4,10 @@ get '/' do
   @name = %w(Amigo Oscar Viking).sample
   erb :index
 end
+
+get '/hello' do
+  @visitor = params[:name]
+  @visitor2 = params[:last_name]
+  #@visitor2 = params[:name]
+  erb :index
+end
