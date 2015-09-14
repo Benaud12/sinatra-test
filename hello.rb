3537911d9d492c5beaf0a494ab4c545dbe7adba8
zsid <zhivko.siderov@gmail.com>
2015-09-14 14:08:43 +0100

    Pucture added

--- hello.rb
+++ hello.rb
@@ -1,13 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "This is a secret page"
-end
-
-get '/bigsecret' do
-  "Shhhh...."
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+    </div>"
 end
